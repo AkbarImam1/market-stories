f1f681a0188784bae201919b93a1de79aeaf75a9
Hans1396 <web.reactjs@hansmatrimony.com>
2024-03-23 21:42:07 +0530

    second commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,106 @@
-import Image from "next/image";
+"use client";
+import React, { useState } from 'react';
+import { Typography, Grid, Avatar, Paper, Button, Drawer, List, ListItem, ListItemIcon, ListItemText, IconButton } from '@mui/material';
+import Layout from './layout';
+import usersData from '../public/user';
+import { AiOutlineEye, AiOutlineComment, AiOutlineShareAlt, AiOutlineHeart, AiFillBell } from 'react-icons/ai';
+import { IoIosArrowForward } from 'react-icons/io';
 
-export default function Home() {
+const Forum = () => {
+  const [open, setOpen] = useState(true);
+
+  const toggleDrawer = () => {
+    setOpen(!open);
+    console.log("sdhuaidsakdajdmalsdk");
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Layout>
+      <Grid container spacing={3}>
+        <Grid item xs={8}>
+          <Typography variant="h4" style={{ marginBottom: '20px' }}>Discussion Forum</Typography>
+        </Grid>
+        <Grid item xs={4}>
+          <Typography variant="h4">Market Stories</Typography>
+        </Grid>
+      </Grid>
+      <Grid container spacing={3}>
+        <Drawer
+          sx={{
+            width: 240,
+            flexShrink: 0,
+            '& .MuiDrawer-paper': {
+              width: 240,
+              boxSizing: 'border-box',
+              backgroundColor: 'darkblue',
+              color: 'white',
+            },
+          }}
+          variant="permanent"
+          anchor="left"
+          open={open}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          <List>
+            {['Hello User', 'Bell', 'Discussion Forum', 'Market Sentiments', 'Sentiment Market', 'Sector', 'Watchlist', 'Events', 'New/Interview'].map((text, index) => (
+              <ListItem button key={text}>
+                <ListItemIcon>{index === 0 ? <AiFillBell /> : null}</ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItem>
+            ))}
+          </List>
+          <IconButton
+  onClick={toggleDrawer}
+  sx={{
+    position: 'absolute',
+    bottom: '20px',
+    left: '50%',
+    transform: 'translateX(-50%)',
+    backgroundColor: 'darkblue',
+    color: 'white',
+    zIndex: 1, // Ensure the button is above the drawer
+  }}
+>
+  <IoIosArrowForward style={{ transform: open ? 'rotate(0deg)' : 'rotate(-90deg)' }} />
+</IconButton>
+        </Drawer>
+        
+        {Array.isArray(usersData.users) && usersData.users.map((user) => (
+          <Grid item xs={12} key={user.name}>
+            <Grid container spacing={3}>
+              <Grid item xs={12} sm={8}>
+                <Paper elevation={3} style={{ padding: '20px', height: '150px', position: 'relative' }}>
+                  <Grid container alignItems="center" spacing={2}>
+                    <Grid item>
+                      <Avatar alt={user.name} src={user.imageUrl} />
+                    </Grid>
+                    <Grid item>
+                      <Typography variant="h5" style={{ fontSize: '16px', marginRight: '10px' }}>{user.name}</Typography>
+                      <Button onClick = {()=>{console.log("hello")}}variant="contained" sx={{ borderRadius: '10px', backgroundColor: 'blue', color: 'white', fontSize: '10px' }} style={{ position: 'relative', left: '80px', bottom: '25px' }}>Sector 2</Button>
+                    </Grid>
+                  </Grid>
+                  <Typography variant="body2" style={{ fontSize: '20px', marginTop: '10px' }}>{user.description}</Typography>
+                  <Typography variant="body2" style={{ fontSize: '12px', color: 'blue', position: 'absolute', top: '5px', right: '5px', marginTop: '7px' }}>{user.time}</Typography>
+                  <div style={{ display: 'flex', justifyContent: 'space-between', marginTop: '30px' }}>
+                    <div><AiOutlineHeart /> 2k</div>
+                    <div><AiOutlineEye /> 2k</div>
+                    <div><AiOutlineComment /> 2k comments</div>
+                    <div><AiOutlineShareAlt /> Share</div>
+                  </div>
+                </Paper>
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <Paper elevation={3} style={{ padding: '20px', height: '75%', width: '50%' }}>
+                  <img src={user.market.imageUrl} alt={user.market.title} style={{ maxWidth: '100%', height: '100px', width: '150px', marginBottom: '10px' }} />
+                  <Typography variant="h6" style={{ fontSize: '14px', marginBottom: '5px' }}>Market: {user.market.title}</Typography>
+                  <Typography variant="body1" style={{ fontSize: '10px', maxHeight: '70px', overflow: 'hidden', position: 'relative', bottom: '10px' }}>{user.market.description}</Typography>
+                </Paper>
+              </Grid>
+            </Grid>
+          </Grid>
+        ))}
+      </Grid>
+    </Layout>
   );
-}
+};
+
+export default Forum;
